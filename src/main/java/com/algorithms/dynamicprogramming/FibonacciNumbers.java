6d1f7c789a402b85e35dc039b894a046281cd06c
kishorejinka <kishorejinka.consult@gmail.com>
2019-10-13 13:34:47 -0700

    recursion - MaxDepthOfBinaryTree dp - FibonnaciNumbers, ClimbingStairs

---
+++ src/main/java/com/algorithms/dynamicprogramming/FibonacciNumbers.java
@@ -0,0 +1,21 @@
+package com.algorithms.dynamicprogramming;
+
+public class FibonacciNumbers {
+
+    public int fib(int N) {
+        int[] dp = new int[N+1];
+        dp[0] = 0;
+        if(N==0) return dp[0];
+        dp[1] = 1;
+        if(N==1) return dp[1];
+        for(int i=2; i<=N; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[N];
+    }
+
+    public static void main(String[] args){
+        FibonacciNumbers fb = new FibonacciNumbers();
+        System.out.println(fb.fib(0));
+    }
+}
