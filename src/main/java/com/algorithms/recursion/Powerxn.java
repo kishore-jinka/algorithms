b7743cd5bc7c1b785b4f8633e1f05e838f1412a1
kishorejinka <kishorejinka.consult@gmail.com>
2019-10-13 22:00:54 -0700

    recursion - MergeTwoSortedLists, Powerxn

---
+++ src/main/java/com/algorithms/recursion/Powerxn.java
@@ -0,0 +1,41 @@
+package com.algorithms.recursion;
+
+//Partially Solved
+public class Powerxn {
+    public double myPow(double x, int n) {
+        if(x == 0 && n < 0) throw new ArithmeticException("Illegal Arithmetic Operation of 1/0");
+        if(x == 0 && n > 0) return 0d;
+        if(x != 0 && n == 0) return 1d;
+        if(x != 0 && n == 1 ) return x;
+        if(x != 0 && n > 1) return x * myPow(x, n-1);
+        if(x != 0 && n < 0) return 1/(myPow(x, Math.abs(n)));
+        return 0;
+    }
+
+    public static void main(String[] args){
+        Powerxn pxn = new Powerxn();
+        System.out.println(pxn.myPow(2, -2));
+    }
+}
+
+/*
+Implement pow(x, n), which calculates x raised to the power n (xn).
+
+Example 1:
+
+Input: 2.00000, 10
+Output: 1024.00000
+Example 2:
+
+Input: 2.10000, 3
+Output: 9.26100
+Example 3:
+
+Input: 2.00000, -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+Note:
+
+-100.0 < x < 100.0
+n is a 32-bit signed integer, within the range [−231, 231 − 1]
+ */
