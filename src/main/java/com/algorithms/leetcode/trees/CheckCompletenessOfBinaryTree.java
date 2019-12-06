e578ac4ef5e79d6105c20f626818e1c199ccef57
kishorejinka <kishorejinka.consult@gmail.com>
2019-12-05 21:12:59 -0800

    trees - CheckCompletenessOfBinaryTree

---
+++ src/main/java/com/algorithms/leetcode/trees/CheckCompletenessOfBinaryTree.java
@@ -0,0 +1,52 @@
+package com.algorithms.leetcode.trees;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 958. Check Completeness of a Binary Tree
+ * https://leetcode.com/problems/check-completeness-of-a-binary-tree/
+ */
+public class CheckCompletenessOfBinaryTree {
+    public boolean isCompleteTree(TreeNode root) {
+        if(root == null) return false;
+        boolean isComplete = true;
+        Queue<RankNode> queue = new LinkedList<RankNode>();
+        int expectedRank = 1;
+        queue.add(new RankNode(root,1));
+        while(!queue.isEmpty()){
+            int numberOfNodes = queue.size();
+            for(int i=0; i<numberOfNodes; i++){
+                RankNode rankNode = queue.poll();
+                if(expectedRank != rankNode.rank) return false;
+                expectedRank++;
+                if(rankNode.getTreeNode().left != null){
+                    queue.add(new RankNode(rankNode.getTreeNode().left, rankNode.getRank()*2));
+                }
+                if(rankNode.getTreeNode().right != null){
+                    queue.add(new RankNode(rankNode.getTreeNode().right, rankNode.getRank()*2 + 1));
+                }
+            }
+
+        }
+        return isComplete;
+    }
+
+    class RankNode{
+        private TreeNode treeNode;
+        private Integer rank;
+
+        public RankNode(TreeNode treeNode, Integer rank) {
+            this.treeNode = treeNode;
+            this.rank = rank;
+        }
+
+        public TreeNode getTreeNode() {
+            return treeNode;
+        }
+
+        public Integer getRank() {
+            return rank;
+        }
+    }
+}
