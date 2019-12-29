6e5b55b7ad2664f5e9b6a57ddc733762629bd320
kishorejinka <kishorejinka.consult@gmail.com>
2019-12-29 01:22:38 -0800

    SortAnArray

---
+++ src/main/java/com/algorithms/leetcode/sorting/SortAnArray.java
@@ -0,0 +1,41 @@
+package com.algorithms.leetcode.sorting;
+
+/**
+ * 912. Sort an Array
+ * https://leetcode.com/problems/sort-an-array/
+ */
+public class SortAnArray {
+
+    public int[] sortArray(int[] nums) {
+        return sort(nums, 0, nums.length -1);
+    }
+
+    private int[] sort(int[] toSort, int startIndex, int endIndex){
+        if(startIndex < endIndex) {
+            int middle = (startIndex + endIndex) / 2;
+            int[] leftSortedArray = sort(toSort, startIndex, middle);
+            int[] rightSortedArray = sort(toSort, middle + 1, endIndex);
+            return merge(leftSortedArray, rightSortedArray);
+        }
+        return new int[]{toSort[startIndex]};
+    }
+
+    private int[] merge(int[] a, int[] b){
+        int[] merged = new int[a.length + b.length];
+        int i = 0, j = 0, m = 0;
+        while(i<a.length && j<b.length){
+            if(a[i] <= b[j]){
+                merged[m++] = a[i++];
+            } else {
+                merged[m++] = b[j++];
+            }
+        }
+        if(i<a.length){
+            while(i<a.length) merged[m++] = a[i++];
+        }
+        if(j<b.length){
+            while(j<b.length) merged[m++] = b[j++];
+        }
+        return merged;
+    }
+}
