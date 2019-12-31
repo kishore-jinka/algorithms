7f4b1eac649225657895534a0c2512f6c1098fb5
kishorejinka <kishorejinka.consult@gmail.com>
2019-12-31 07:51:30 -0800

    K largest and smallest problems

---
+++ src/main/java/com/algorithms/leetcode/searching/KClosestPointsToOrigin.java
@@ -0,0 +1,13 @@
+package com.algorithms.leetcode.searching;
+
+/**
+ * 973. K Closest Points to Origin
+ * https://leetcode.com/problems/k-closest-points-to-origin/
+ */
+public class KClosestPointsToOrigin {
+
+    public int[][] kClosest(int[][] points, int K) {
+        return null;
+    }
+
+}
---
+++ src/main/java/com/algorithms/leetcode/searching/KthLargestElementInAnArray.java
@@ -0,0 +1,12 @@
+package com.algorithms.leetcode.searching;
+
+/**
+ * 215. Kth Largest Element in an Array
+ * https://leetcode.com/problems/kth-largest-element-in-an-array/
+ * QuickSelect algorithm to find (n-k) smallest element
+ */
+public class KthLargestElementInAnArray {
+    public int findKthLargest(int[] nums, int k) {
+        return 0;
+    }
+}
