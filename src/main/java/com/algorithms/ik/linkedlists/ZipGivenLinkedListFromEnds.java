7be5c2d34643a3861fe345aa0883220fb5b5ea43
kishorejinka <kishorejinka.consult@gmail.com>
2020-01-21 09:29:22 -0800

    ZipGivenLinkedListFromEnds

---
+++ src/main/java/com/algorithms/ik/linkedlists/ZipGivenLinkedListFromEnds.java
@@ -0,0 +1,32 @@
+package com.algorithms.ik.linkedlists;
+
+
+/**
+ * TODO: INCOMPLETE
+ */
+public class ZipGivenLinkedListFromEnds {
+
+    static LinkedListNode zip_given_linked_list(LinkedListNode head) {
+
+
+        return null;
+
+    }
+
+}
+
+/*
+For input n = 6 and L: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> NULL, output will be:
+
+1
+
+6
+
+2
+
+5
+
+3
+
+4
+ */
