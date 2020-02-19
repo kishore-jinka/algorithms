7986e4dd893555a40cbd7205437a7b8e2ccb074d
kishorejinka <kishorejinka.consult@gmail.com>
2020-02-18 23:10:29 -0800

    RemoveKDigits

--- src/main/java/com/algorithms/leetcode/greedy/RemoveKDigits.java
+++ src/main/java/com/algorithms/leetcode/greedy/RemoveKDigits.java
@@ -3,57 +3,39 @@ package com.algorithms.leetcode.greedy;
 /**
  * 402. Remove K Digits
  * https://leetcode.com/problems/remove-k-digits/
- * //INCOMPLETE
  */
 public class RemoveKDigits {
+
     public String removeKdigits(String num, int k) {
-        if(k == 0) return num;
-        String toReturn = "0";
-        if(k >= num.length()) return toReturn;
-        if(k == 1){
-            if(num.length() > 2 && num.charAt(1) == '0') return num.substring(2);
-            int highestNumber = 0;
-            int position = -1;
-            for (int i = 0; i < num.length(); i++) {
-                int digit = Integer.parseInt(num.substring(i, i + 1));
-                if (digit > highestNumber){
-                    highestNumber = digit;
-                    position = i;
-                }
-            }
-            if(position == num.length() - 1){
-                return num.substring(0, position);
-            }else if(position == 0){
-                return num.substring(1);
-            }else{
-                return num.substring(0,position) + num.substring(position+1);
-            }
-
-        }
-        if(k > 1 && k < num.length()) {
-            int lowestNumber = 9;
-            for (int i = 0; i <= k; i++) {
-                int digit = Integer.parseInt(num.substring(i, i + 1));
-                if (digit < lowestNumber) lowestNumber = digit;
-            }
-            if(k+1 == num.length()){
-                toReturn = String.valueOf(lowestNumber);
-            }else {
-                toReturn = num.substring(k + 1);
-                if (lowestNumber > 0) toReturn = lowestNumber + toReturn;
-            }
-            return toReturn;
-        }
-
-        return toReturn;
+        return removeKdigitsStringBuilder(new StringBuilder(num), k).toString();
     }
 
-    private String removeLeadingZero(String num){
+    public StringBuilder removeKdigitsStringBuilder(StringBuilder num, int k) {
+        if(k == 0) return removeLeadingZero(num);
+        if(k >= num.length()) return new StringBuilder("0");
+        int flipIndex = getFlixIndex(num);
+        return removeKdigitsStringBuilder(num.deleteCharAt(flipIndex), k-1);
+    }
 
+    private int getFlixIndex(StringBuilder num){
+        for(int i=0; i<num.length() - 1; i++){
+            char numI = num.charAt(i);
+            char numNext = num.charAt(i+1);
+            if(numI > numNext) return i;
+        }
+        return num.length() - 1;
+    }
+
+    private StringBuilder removeLeadingZero(StringBuilder num){
+        if(num.length() > 1 && num.substring(0,1).equals("0")){
+            return removeLeadingZero(num.deleteCharAt(0));
+        }else{
+            return num;
+        }
     }
 
     public static void main(String[] args){
-        String num = "1173";
+        String num = "10";
         int k = 2;
         RemoveKDigits removeDigits = new RemoveKDigits();
         System.out.println(removeDigits.removeKdigits(num,k));
