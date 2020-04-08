9dd70963e97a16f0b2a964830bfe79af944bfd91
kishorejinka <kishorejinka.consult@gmail.com>
2020-04-08 12:12:36 -0700

    MiddleOfLinkedList

--- src/main/java/com/algorithms/leetcode/linkedlists/MiddleOfLinkedList.java
+++ src/main/java/com/algorithms/leetcode/linkedlists/MiddleOfLinkedList.java
@@ -1,5 +1,10 @@
 package com.algorithms.leetcode.linkedlists;
 
+/**
+ * 876. Middle of the Linked List
+ * https://leetcode.com/problems/middle-of-the-linked-list/
+ * https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/529/week-2/3290/
+ */
 public class MiddleOfLinkedList {
     public ListNode middleNode(ListNode head) {
         ListNode fastPointer = head;
@@ -23,13 +28,13 @@ public class MiddleOfLinkedList {
         ListNode node3 = new ListNode(3);
         ListNode node4 = new ListNode(4);
         ListNode node5 = new ListNode(5);
-        ListNode node6 = new ListNode(6);
+        //ListNode node6 = new ListNode(6);
 
         node1.next = node2;
         node2.next = node3;
         node3.next = node4;
         node4.next = node5;
-        node5.next = node6;
+        //node5.next = node6;
 
         MiddleOfLinkedList middleOfLinkedList = new MiddleOfLinkedList();
         System.out.println(middleOfLinkedList.middleNode(node1).val);
