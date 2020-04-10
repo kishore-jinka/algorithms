aaf517c955cd7a299c2a6538e4db3988475c5ed4
kishorejinka <kishorejinka.consult@gmail.com>
2020-04-10 13:15:24 -0700

    KthLargestElementInAnArray

--- src/main/java/com/algorithms/leetcode/searching/KthLargestElementInAnArray.java
+++ src/main/java/com/algorithms/leetcode/searching/KthLargestElementInAnArray.java
@@ -3,12 +3,12 @@ package com.algorithms.leetcode.searching;
 /**
  * 215. Kth Largest Element in an Array
  * https://leetcode.com/problems/kth-largest-element-in-an-array/
- * QuickSelect algorithm to find n-k smallest element
- * TODO: INCOMPLETE
+ * QuickSelect algorithm to find n-k+1 smallest element
  */
 public class KthLargestElementInAnArray {
     public int findKthLargest(int[] nums, int k) {
-        return findKthSmallest(nums, nums.length - k , 0, nums.length-1);
+        if(k<1 || k>nums.length) return Integer.MIN_VALUE;
+        return findKthSmallest(nums, nums.length - k + 1 , 0, nums.length-1);
     }
 
     private int findKthSmallest(int[] nums, int k, int start, int end){
@@ -20,7 +20,7 @@ public class KthLargestElementInAnArray {
     }
 
     private int findLomutosParitionIndex(int[] nums, int start, int end){
-        int orangeIndex = -1;
+        int orangeIndex = start-1;
         int pivot = nums[end];
         for(int greenIndex = start; greenIndex <= end; greenIndex ++){
             if(nums[greenIndex] <= pivot){
@@ -34,20 +34,9 @@ public class KthLargestElementInAnArray {
     }
 
     public static void main(String[] args){
-        //int[] a = {3,1,2,4};
-        //int[] a = {3,2,1,5,6,4};
-        //int[] a = {3,2,3,1,2,4,5,5,6};
-        //int k = 2;
-        //int[] a = {7,6,5,4,3,2,1};
-        //2
         int[] a = {64, 25, 12, 22, 23};
         KthLargestElementInAnArray myClass = new KthLargestElementInAnArray();
-        System.out.println(myClass.findKthSmallest(a, 4, 0, a.length - 1));
-//        System.out.println(myClass.findLomutosParitionIndex(a,0, a.length - 1));
-//        for(int i : a){
-//            System.out.println(i);
-//        }
+        //System.out.println(myClass.findKthSmallest(a, 5, 0, a.length - 1));
+        System.out.println(myClass.findKthLargest(a, 5));
     }
 }
-
-//System.out.println(myClass.findKthLargest(a, k));
