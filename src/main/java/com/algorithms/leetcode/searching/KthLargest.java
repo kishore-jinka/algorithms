943862c1b71809a909fc995c7fac3cbd5c16f41f
kishorejinka <kishorejinka.consult@gmail.com>
2020-04-11 18:46:02 -0700

    KthLargest

--- src/main/java/com/algorithms/leetcode/searching/KthLargest.java
+++ src/main/java/com/algorithms/leetcode/searching/KthLargest.java
@@ -45,19 +45,3 @@ public class KthLargest {
         System.out.println(kthLargest.add(4));   // returns 8
     }
 }
-
-//    int k = 3;
-//    int[] arr = [4,5,8,2];
-//    KthLargest kthLargest = new KthLargest(3, arr);
-//    kthLargest.add(3);   // returns 4
-//    kthLargest.add(5);   // returns 5
-//    kthLargest.add(10);  // returns 5
-//    kthLargest.add(9);   // returns 8
-//    kthLargest.add(4);   // returns 8
-
-
-/**
- * Your KthLargest object will be instantiated and called as such:
- * KthLargest obj = new KthLargest(k, nums);
- * int param_1 = obj.add(val);
- */
