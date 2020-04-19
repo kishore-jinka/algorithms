5051b9192af0af5e722db715a6f66f42bd01f5fc
kishorejinka <kishorejinka.consult@gmail.com>
2020-04-19 11:42:37 -0700

    ValidMountainArray

---
+++ src/main/java/com/algorithms/leetcode/arrays/ValidMountainArray.java
@@ -0,0 +1,37 @@
+package com.algorithms.leetcode.arrays;
+
+import com.algorithms.leetcode.strings.ValidParenthesisString;
+
+/**
+ * 941. Valid Mountain Array
+ * https://leetcode.com/problems/valid-mountain-array/
+ * https://leetcode.com/explore/learn/card/fun-with-arrays/527/searching-for-items-in-an-array/3251/
+ */
+public class ValidMountainArray {
+    public boolean validMountainArray(int[] A) {
+        if(A.length < 3) return false;
+        boolean increasing = true;
+        for(int i=1; i<A.length; i++){
+            if(A[i] == A[i-1]) return false;
+            if(increasing) {
+                if (A[i] < A[i-1]){
+                    if(i==1) return false;
+                    increasing = false;
+                }
+            }else{
+                if (A[i] > A[i-1]){
+                    return false;
+                }
+            }
+        }
+        if(increasing) return false;
+        return true;
+    }
+
+    public static void main(String[] args){
+        int[] A = {0,2,3,3,5,2,1,0};
+        ValidMountainArray validMountainArray = new ValidMountainArray();
+        System.out.println(validMountainArray.validMountainArray(A));
+    }
+}
+
