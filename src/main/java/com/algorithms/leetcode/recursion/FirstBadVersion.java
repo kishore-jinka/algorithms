1dcba16cba8aad7090b3961b360ca8db41204b33
Jinka, Kishore Kumar <kishore.kumar.jinka@one.verizon.com>
2020-05-01 11:15:15 -0700

    278 First Bad Version

---
+++ src/main/java/com/algorithms/leetcode/recursion/FirstBadVersion.java
@@ -0,0 +1,42 @@
+package com.algorithms.leetcode.recursion;
+
+/**
+ * 278. First Bad Version
+ * https://leetcode.com/problems/first-bad-version/
+ * https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3316/
+ * Binary Search Variant
+ */
+public class FirstBadVersion extends VersionControl{
+    public int firstBadVersion(int n) {
+        return getBadVersionRecursive(1, n);
+    }
+
+    private int getBadVersionRecursive(int start, int end){
+        if(start < end){
+            int mid = start/2 + end/2;
+            if(start%2 == 1 && end%2 == 1) mid = mid + 1;
+            if(isBadVersion(mid)){ // bad version is between start and mid
+                return getBadVersionRecursive(start, mid);
+            }else{ // bad version is between mid and end
+                return getBadVersionRecursive(mid+1, end);
+            }
+        }
+        return start;
+    }
+
+    public static void main(String[] args){
+        FirstBadVersion firstBadVersion = new FirstBadVersion();
+        System.out.println(firstBadVersion.firstBadVersion(2126753390));
+    }
+}
+
+/**
+ * VersionControl is implemented in custom way.
+ * The actual implementation is with leetcode.
+ */
+class VersionControl{
+    boolean isBadVersion(int version){
+        if(version >= 1702766719) return true;
+        return false;
+    }
+}
