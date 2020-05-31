9170b055b9e3302c972c2734de3ad382a649a6c7
kishorejinka <kishorejinka.consult@gmail.com>
2020-05-30 18:25:40 -0700

    Search Insert Position

---
+++ src/main/java/com/algorithms/leetcode/searching/SearchInsertPosition.java
@@ -0,0 +1,19 @@
+package com.algorithms.leetcode.searching;
+
+/**
+ * 35. Search Insert Position
+ * https://leetcode.com/problems/search-insert-position/
+ */
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(target == nums[mid]) return mid;
+            if(target < nums[mid]) end = mid - 1;
+            if(target > nums[mid]) start = mid + 1;
+        }
+        return start;
+    }
+}
