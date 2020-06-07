3f4b1fbea5d1dc558b37cce0e7b3114ca2750722
kishorejinka <kishorejinka.consult@gmail.com>
2020-06-06 20:26:56 -0700

    First And Last Position Refactored

--- src/main/java/com/algorithms/leetcode/searching/FirstAndLastPosition.java
+++ src/main/java/com/algorithms/leetcode/searching/FirstAndLastPosition.java
@@ -6,37 +6,36 @@ package com.algorithms.leetcode.searching;
  */
 public class FirstAndLastPosition {
     public int[] searchRange(int[] nums, int target) {
-        int position = getPosition(nums, target);
-        int[] toReturn = {-1, -1};
-        if(position == -1) return toReturn;
-        int frontIndex = position;
-        int rearIndex = position;
-        while(frontIndex >= 0 && target == nums[frontIndex]){
-            frontIndex--;
-        }
-        while(rearIndex <= nums.length - 1 && target == nums[rearIndex]){
-            rearIndex++;
-        }
-        toReturn[0] = frontIndex+1;
-        toReturn[1] = rearIndex-1;
-        return  toReturn;
-    }
-
-    private int getPosition(int[] nums, int target) {
         int start = 0;
         int end = nums.length - 1;
+        int[] positions = {-1, -1};
         while(start <= end){
             int mid = start + (end - start)/2;
-            if(target == nums[mid]) return mid;
-            if(target < nums[mid]) end = mid - 1;
-            if(target > nums[mid]) start = mid + 1;
+            if(nums[mid] >= target){
+                end = mid - 1;
+            } else { //<
+                start = mid + 1;
+            }
         }
-        return -1;
+        if (start == nums.length || nums[start] != target) return positions;
+        int first = start;
+        positions[0] = first;
+        end = nums.length - 1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] > target){
+                end = mid - 1;
+            } else { //<=
+                start = mid + 1;
+            }
+        }
+        positions[1] = end;
+        return positions;
     }
 
     public static void main(String[] args){
         int[] nums = {5,7,7,8,8,10};
-        int target = 10;
+        int target = 8;
         FirstAndLastPosition firstAndLastPosition = new FirstAndLastPosition();
         int[] range = firstAndLastPosition.searchRange(nums, target);
         System.out.println(range[0] + "," + range[1]);
