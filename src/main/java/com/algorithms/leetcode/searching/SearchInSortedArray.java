6df303013dc0d0eb543fe8461e326d3aa0f05da3
kishorejinka <kishorejinka.consult@gmail.com>
2020-06-08 23:22:58 -0700

    Search In Sorted Array of Unknown Size

---
+++ src/main/java/com/algorithms/leetcode/searching/SearchInSortedArray.java
@@ -0,0 +1,27 @@
+package com.algorithms.leetcode.searching;
+
+/**
+ * 702. Search in a Sorted Array of Unknown Size
+ * https://leetcode.com/problems/search-in-a-sorted-array-of-unknown-size/
+ */
+public class SearchInSortedArray {
+    public int search(ArrayReader reader, int target) {
+        int start = 0;
+        int end = 1;
+        while(reader.get(end) < target){
+            start = end;
+            end = end * 2;
+        }
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(reader.get(mid) == target) return mid;
+            if(reader.get(mid) < target) start = mid + 1;
+            if(reader.get(mid) > target) end = mid - 1;
+        }
+        return -1;
+    }
+}
+
+interface ArrayReader {
+    public int get(int index);
+}
