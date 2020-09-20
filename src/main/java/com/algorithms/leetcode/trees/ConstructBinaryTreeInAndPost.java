d0646bc1c90a5a9304e47c7b86404885be3d996e
kishorejinka.consult@gmail.com <kishorejinka.consult@gmail.com>
2020-09-20 12:18:09 -0700

    ConstructBinaryTreeInAndPost

--- src/main/java/com/algorithms/leetcode/trees/ConstructBinaryTreeInAndPost.java
+++ src/main/java/com/algorithms/leetcode/trees/ConstructBinaryTreeInAndPost.java
@@ -1,57 +1,31 @@
 package com.algorithms.leetcode.trees;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- *
- * TODO: INCOMPLETE
  * 106. Construct Binary Tree from Inorder and Postorder Traversal
  * https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
- * For example, given
- *
- * inorder = [9,3,15,20,7]
- * postorder = [9,15,7,20,3]
- * Return the following binary tree:
- *
- *     3
- *    / \
- *   9  20
- *     /  \
- *    15   7
  */
 public class ConstructBinaryTreeInAndPost {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        return buildSubTree(inorder, 0, inorder.length-1, postorder, 0, postorder.length-1);
+        if(postorder == null || inorder == null || postorder.length != inorder.length) return null;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<inorder.length; i++) map.put(inorder[i], i);
+        return constructRecursive(postorder, 0, postorder.length - 1, inorder, 0, inorder.length - 1, map);
     }
 
-    private TreeNode buildSubTree(int[] inorder, int inorderStartIndex, int inorderEndIndex, int[] postorder, int postorderStartIndex, int postorderEndIndex){
-        TreeNode root = null;
-        int rootValue = postorder[postorderEndIndex];
-        root = new TreeNode(rootValue);
-        int inorderIndexOfRoot = findIndex(inorder, rootValue);
-        int lstInorderStartIndex = inorderStartIndex;
-        int lstInorderEndIndex = inorderIndexOfRoot-1;
-        int lstPostorderStartIndex = postorderStartIndex;
-        int lstPostorderEndIndex = inorderIndexOfRoot-1;
-
-        int rstInorderStartIndex = inorderIndexOfRoot+1;
-        int rstInorderEndIndex = inorderEndIndex;
-        int rstPostorderStartIndex = lstPostorderEndIndex+1;
-        int rstPostorderEndIndex = postorderEndIndex-1;
-
-        TreeNode leftSubTreeRoot = null;
-        if(lstInorderEndIndex >= lstInorderStartIndex && lstPostorderEndIndex >=lstPostorderStartIndex)
-            leftSubTreeRoot = buildSubTree(inorder, lstInorderStartIndex, lstInorderEndIndex, postorder, lstPostorderStartIndex, lstPostorderEndIndex);
-        TreeNode rightSubTreeRoot = null;
-        if(rstInorderEndIndex >= rstInorderStartIndex && rstPostorderEndIndex >=rstPostorderStartIndex)
-            rightSubTreeRoot = buildSubTree(inorder, rstInorderStartIndex, rstInorderEndIndex, postorder, rstPostorderStartIndex, rstPostorderEndIndex);
-        root.left = leftSubTreeRoot;
-        root.right = rightSubTreeRoot;
-        return root;
-    }
-
-    private int findIndex(int[] someorder, int value){
-        for(int i=0;i<someorder.length; i++){
-            if(someorder[i] == value) return i;
+    private TreeNode constructRecursive(int[] postorder, int postStart, int postEnd, int[] inorder, int inStart, int inEnd, Map<Integer, Integer> inorderMap){
+        if(postStart > postEnd){
+            return null;
+        }else if(postStart == postEnd){
+            return new TreeNode(postorder[postEnd]);
         }
-        return -1;
+        TreeNode root = new TreeNode(postorder[postEnd]);
+        int inorderRootIndex = inorderMap.get(root.val);
+        int numberLeft = inorderRootIndex - inStart;
+        root.left = constructRecursive(postorder, postStart, postStart + numberLeft - 1, inorder, inStart, inorderRootIndex - 1, inorderMap);
+        root.right = constructRecursive(postorder, postStart + numberLeft, postEnd - 1, inorder, inorderRootIndex + 1, inEnd, inorderMap);
+        return root;
     }
 }
